edc36cb6b1bab15aaa0bba42474350f40852f31f
Elmar Quluzade <elmarguluzade1@gmail.com>
2023-03-28 10:10:16 +0400

    react-todo-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+ const [value, setValue] = useState<string> ('');
+ const [addValue, setAddValue] = useState<string[]>([]);
+ const [errorMessage, setErrorMessage] = useState<string>('');
+//  const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
+ 
+ const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+ }
+
+ const handleAddItem = () => {
+  if (value.trim() === ''){
+    setErrorMessage("Please enter a valid value!");
+    setTimeout(() => {
+      setErrorMessage("");
+    },3000)
+    return;
+   }
+   setAddValue([...addValue,value]);
+  //  setCheckedItems([...checkedItems,false]);
+   setValue('');
+   setErrorMessage('');
+ }
+
+ const handleAllDelete = () => {
+      
+  setAddValue([]);
+  // setCheckedItems([]);
+
+ }
+
+//  const handleItemDelete = () => {
+//     const filteredItems = addValue.filter((item,index) => !checkedItems[index]);
+//     const filteredCheckedItems = checkedItems.filter((item,index) => !checkedItems[index]);
+//     setAddValue(filteredItems);
+//     setCheckedItems(filteredCheckedItems);
+//  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+       <div className='todo-app '>
+     
+        <div>
+        <input 
+         type="text" 
+         value={value} 
+         onChange= {handleChange} 
+         className='mt-5  todo-input' />
+         <div className='h-5'>
+         {errorMessage && <div className=' text-red-500 font-extralight' >{errorMessage}</div>}
+         </div>
+        </div>
+        <div>
+          <button className='delete-btn'>delete</button>
+          <button onClick={handleAddItem} className='add-btn'>add</button>
+        </div>
+        <div className='todo-list'>
+           <div className='todo-list-item'>
+              <ul className='mt-5'>
+              
+                {addValue.map((item, index) => (
+                  <li className='flex' key={index}>
+                 <input 
+                 type="checkbox" 
+                //  checked = {checkedItems[index]}
+                 className='todo-checkbox'/>{item}
+                 </li>
+                ))}
+                  
+              </ul>
+           </div>
+        </div>
+        <div>
+          <button onClick={handleAllDelete} className='all-delete'>ALL DELETE</button>
+        </div>
+       </div>
+      
       </header>
     </div>
   );
